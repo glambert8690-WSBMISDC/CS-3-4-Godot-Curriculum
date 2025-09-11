e828720557f07792ae55736f954b556f70f89802
glambert8690-WSBMISDC <glambert8690@student.pps.net>
2025-09-11 14:06:47 -0700

    I remade the coin and got a github account

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -3,11 +3,36 @@
 @tool
 
 extends Area2D
+class_name coin
+
+# Healing properties
+@export var coin_amount: int = 100
+@export var auto_pickup: bool = true
+
+func _ready():
+	print("Coin created - Coins " + str(coin_amount) + "CP")
+	# Connect the collision signal
+	body_entered.connect(_on_body_entered)
+
+func _on_body_entered(body):
+	# Check if it's the player
+	if body is Player:
+		print("Player found Coin!")
+		
+		# Try to heal the player (will work once they add heal method)
+		if body.has_method("coin"):
+			body.coin(coin_amount)
+			print("Player coined for " + str(coin_amount) + "  CP!")
+			
+			# Remove the potion after use
+			if auto_pickup:
+				$AnimationPlayer.play("disappear")
+		else:
+			print("Player doesn't have coin() method yet - coming in Lesson 2!")
+			if auto_pickup:
+				$AnimationPlayer.play("disappear")
 
 
-@export var color: Color
 
-
-func _process(delta: float) -> void:
 	# Set the color of the coin
-	$AnimatedSprite2D.material.set_shader_parameter("color", color)
+	
